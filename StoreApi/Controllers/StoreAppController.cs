8ae2e450cd74fd5d4eacfd77af5ad8d5c9943b30
Brian Dawkins <brian.dawkins@reature.net>
2022-02-21 12:52:00 -0500

    Program.cs/Controller start

---
+++ StoreApi/Controllers/StoreAppController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreBL;
+
+/*
+    Auto generate by utilizing aspnet-codegenerator tool
+    https://docs.microsoft.com/en-us/aspnet/core/fundamentals/tools/dotnet-aspnet-codegenerator?view=aspnetcore-6.0
+
+    -To start
+    --Install tool first = dotnet tool install -g dotnet-aspnet-codegenerator
+    --Add package to api project = dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+    
+    --To create a controller
+    dotnet aspnet-codegenerator controller -name StoreApp -api -outDir Controllers -actions
+
+    " dotnet aspnet-codegenerator controller" - creates a controller 
+
+    "-name {NameOfController}" - name the controller to whatever you put
+
+    "-api" - Makes the controller restful style api
+
+    "-outDir Controllers" - Puts the controller inside the controller folder in api project
+
+    "-action" - Adds in action(methods) in your controller
+*/
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreAppController : ControllerBase
+    {
+        private IStoreFrontBL _storeBL;
+        public StoreAppController(IStoreFrontBL p_storeBL)
+        {
+            _storeBL = p_storeBL;
+        }
+
+        // GET: api/StoreApp
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            try
+            {
+                return Ok(_storeBL.GetAllCustomers());
+            }
+            catch (SqlException)
+            {
+                //The API is responsible for sending the right resource and the right status code
+                //In this case if it was unable to connect to the database, it will give a 404 status code
+                return NotFound();
+            }
+        }
+
+        // GET: api/StoreApp/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/StoreApp
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/StoreApp/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/StoreApp/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- StoreApi/Program.cs
+++ StoreApi/Program.cs
@@ -1,3 +1,6 @@
+using StoreAppDL;
+using StoreBL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddScoped<IRepository>(repo => new SQLRepository(builder.Configuration.GetConnectionString("Reference2DB")));
+builder.Services.AddScoped<IStoreFrontBL, StoreFrontBL>();
 
 var app = builder.Build();
 
